572fa36604008214da9209b71ae63806af9cd50e
Pedro Alves <pedro.alves@webdetails.pt>
2012-02-16 01:50:29 +0000

    - Added samplePanels - Fixed css issues

--- html/samplePanels.js
+++ html/samplePanels.js
@@ -67,3 +67,68 @@ wd.caf.impl.keybinds.about = function(spec){
 
 wd.caf.registry.registerPanel( wd.caf.impl.panels.about() );
 wd.caf.registry.registerKeybind( wd.caf.impl.keybinds.about() );
+
+
+
+/**
+ * About panel
+ * @class
+ *
+ */
+wd.caf.impl.panels.panels = function(spec){
+    
+    /**
+     * Specific specs
+     */
+    
+    
+    var _spec = {
+        name: "panels",
+        description: "Panels",
+        order: 1,
+        color: "blue"
+    };
+
+        
+    var myself = wd.caf.panel($.extend({},_spec,spec));
+    //var myself = wd.caf.impl.panels.underConstruction($.extend({},_spec,spec))
+
+
+    /**
+     * Describes this interface
+     * @name panel.init
+     * @memberof wd.caf.panel
+     */
+    myself.draw = function($ph){
+        
+        var d = $('<div/>').addClass("underConstruction").text("Welcome to CDE2");
+        $ph.append(d);
+    }
+
+
+    return myself;
+        
+};
+
+
+wd.caf.impl.keybinds.panels = function(spec){
+    
+     /**
+     * Specific specs
+     */
+    var _spec = {
+        name: "panels",
+        description: "Panels",
+        key: "0",
+        order: 0
+    };
+
+    var myself = wd.caf.impl.keybinds.panelKeybind($.extend({},_spec,spec));
+    return myself;
+        
+};
+
+wd.caf.registry.registerPanel( wd.caf.impl.panels.panels() );
+wd.caf.registry.registerKeybind( wd.caf.impl.keybinds.panels() );
+
+
